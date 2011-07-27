55100fde4bde423eb64114ee3248336754fc3a76
chen.sst <chen.sst@b90aed45-4297-9963-ff9d-98b604ac09a9>
2011-07-27 07:17:54 +0000

    Import a copy of weasel 0.3c source code.

--- librime/shell.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if exist env.bat call env.bat
-set OLD_PATH=%PATH%
-if defined DEV_PATH set PATH=%DEV_PATH%;%OLD_PATH%
-cmd
--- librime/test/rime_test_main.cc
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include <rime/component.h>
-
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  rime::RegisterRimeComponents();
-  return RUN_ALL_TESTS();
-}
