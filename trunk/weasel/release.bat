a41fa7d24d7aeb1f9282687f962c1b70b60ec6eb
chen.sst <chen.sst@b90aed45-4297-9963-ff9d-98b604ac09a9>
2011-09-01 01:48:22 +0000

    Removed dependency on Python for Weasel 0.9.

--- trunk/weasel/release.bat
+++ trunk/weasel/release.bat
@@ -5,16 +5,16 @@ set SERVER=%OUTPUT%\WeaselServer.exe
 if exist %SERVER% %SERVER% /q
 rmdir /s /q %OUTPUT%
 mkdir %OUTPUT%
-mkdir %OUTPUT%\data
-copy ..\rime.py\data\*.bat %OUTPUT%\data
-copy ..\rime.py\data\*.py %OUTPUT%\data
-copy ..\rime.py\data\*.txt %OUTPUT%\data
-mkdir %OUTPUT%\engine
-mkdir %OUTPUT%\engine\ibus
-copy ..\rime.py\engine\*.py %OUTPUT%\engine
-copy ..\rime.py\weasel\*.py %OUTPUT%\engine
-copy ..\rime.py\weasel\ibus\*.py %OUTPUT%\engine\ibus
-copy misc\*.conf %OUTPUT%\engine
+rem mkdir %OUTPUT%\data
+rem copy ..\rime.py\data\*.bat %OUTPUT%\data
+rem copy ..\rime.py\data\*.py %OUTPUT%\data
+rem copy ..\rime.py\data\*.txt %OUTPUT%\data
+rem mkdir %OUTPUT%\engine
+rem mkdir %OUTPUT%\engine\ibus
+rem copy ..\rime.py\engine\*.py %OUTPUT%\engine
+rem copy ..\rime.py\weasel\*.py %OUTPUT%\engine
+rem copy ..\rime.py\weasel\ibus\*.py %OUTPUT%\engine\ibus
+rem copy misc\*.conf %OUTPUT%\engine
 copy misc\*.bat %OUTPUT%
 copy misc\*.js %OUTPUT%
 copy misc\*.py %OUTPUT%
