4e737268c96b2b3682cc14316b887dfe8f660adc
chen.sst <chen.sst@b90aed45-4297-9963-ff9d-98b604ac09a9>
2011-09-28 06:30:15 +0000

    布局没有最好只有更好

--- typewriter/script/zhung.js
+++ typewriter/script/zhung.js
@@ -14,12 +14,12 @@ var Zhung = new Class({
     },
     */
     green: {
-      'q': 'ㄈ', 'w': 'ㄇ<sub>ㄢ</sub>', 'e': 'ㄆ<sub>ㄡ</sub>', 'r': 'ㄅ<sub>ㄠ</sub>', 't': 'ㄪ',
-      'y': 'ㄩ', 'u': 'ㄉ<sub>ㄞ</sub>', 'i': 'ㄊ<sub>ㄟ</sub>', 'o': 'ㄋ<sub>ㄣ</sub>', 'p': 'ㄌ',
-      'a': 'ㄚ', 's': '<sup>ㄏㄒ</sup><sub>ㄛ</sub>', 'd': '<sup>ㄎㄑ</sup><sub>ㄜ</sub>', 'f': '<sup>ㄍㄐ</sup><sub>ㄝ</sub>', 'g': 'ㄨ',
-      'h': 'ㄧ', 'j': 'ㄓ<sub>ˆ</sub>', 'k': 'ㄔ<sub>ˊ</sub>', 'l': 'ㄕ<sub>ˋ</sub>', 
-      'z': 'ㄦ', 'x': 'ㄙ', 'c': 'ㄘ', 'v': 'ㄗ', 'b': 'ㄤ',
-      'n': 'ㄥ', 'm': 'ㄖ' 
+      'q': 'ㄪ', 'w': 'ㄈ<sub>ㄤ</sub>', 'e': 'ㄇ<sub>ㄢ</sub>', 'r': 'ㄆ<sub>ㄡ</sub>', 't': 'ㄅ<sub>ㄠ</sub>',
+      'y': 'ㄉ<sub>ㄞ</sub>', 'u': 'ㄊ<sub>ㄟ</sub>', 'i': 'ㄋ<sub>ㄣ</sub>', 'o': 'ㄌ<sub>ㄥ</sub>', /* 'p': '', */
+      'a': 'ㄏ<sub>ㄚ</sub>', 's': 'ㄎ<sub>ㄛ</sub>', 'd': 'ㄍ<sub>ㄜ</sub>', 'f': 'ㄨ', 'g': 'ㄝ',
+      'h': 'ㄩ', 'j': 'ㄧ', 'k': 'ㄗ<sub>ˊ</sub>', 'l': 'ㄘ<sub>ˋ</sub>',
+      /* 'z': '', */ 'x': 'ㄖ', 'c': 'ㄕ', 'v': 'ㄔ', 'b': 'ㄓ',
+      'n': 'ㄦ<sub>ˆ</sub>', 'm': 'ㄙ<sub>˙</sub>' 
     },
     red: {
       'space': ' '
@@ -66,28 +66,30 @@ var Zhung = new Class({
 
     // shengdiau
     if (!this._slot_is_empty() && 
-        ((key == 'space' || key == 'j' || key == 'k' || key == 'l'))) {
+        ((key == 'space' || key == 'm' || key == 'k' || key == 'l' || key == 'n'))) {
       if (key != 'space')
         this._fill_slot(3, key_name.charAt(key_name.length - 1));
       if (this._slot[0] && !this._slot[1] && !this._slot[2]) {
         if (this._slot[0] == 'ㄅ') this._slot[0] = 'ㄠ';
         else if (this._slot[0] == 'ㄆ') this._slot[0] = 'ㄡ';
         else if (this._slot[0] == 'ㄇ') this._slot[0] = 'ㄢ';
+        else if (this._slot[0] == 'ㄈ') this._slot[0] = 'ㄤ';
         else if (this._slot[0] == 'ㄉ') this._slot[0] = 'ㄞ';
         else if (this._slot[0] == 'ㄊ') this._slot[0] = 'ㄟ';
         else if (this._slot[0] == 'ㄋ') this._slot[0] = 'ㄣ';
-        else if (this._slot[0] == 'ㄍ') this._slot[0] = 'ㄝ';
-        else if (this._slot[0] == 'ㄎ') this._slot[0] = 'ㄜ';
-        else if (this._slot[0] == 'ㄏ') this._slot[0] = 'ㄛ';
+        else if (this._slot[0] == 'ㄌ') this._slot[0] = 'ㄥ';
+        else if (this._slot[0] == 'ㄍ') this._slot[0] = 'ㄜ';
+        else if (this._slot[0] == 'ㄎ') this._slot[0] = 'ㄛ';
+        else if (this._slot[0] == 'ㄏ') this._slot[0] = 'ㄚ';
       }
       var s = this._slot.join('');
       sink(s);
       this._clear_buffer();
     }
     // jaiyin
-    else if (key == 'h' || key == 'g' || key == 'y') {
+    else if (key == 'f' || key == 'j' || key == 'h') {
       this._fill_slot(1, key_name.charAt(0));
-      if (key == 'h' || key == 'y') {
+      if (key == 'j' || key == 'h') {
         var shengmu = this._slot[0];
         if (shengmu == 'ㄍ') this._fill_slot(0, 'ㄐ');
         else if (shengmu == 'ㄎ') this._fill_slot(0, 'ㄑ');
@@ -96,12 +98,15 @@ var Zhung = new Class({
     }
     // shengmu
     else if (this._slot_is_empty() && 
-             key != 'a' && key != 'b' && key != 'n' && key != 'z') {
+             key != 'g' && key != 'n') {
       this._fill_slot(0, key_name.charAt(0));
     }
     // yunmu
     else {
-      this._fill_slot(2, key_name.charAt(key_name.length - 1));
+      if (key == 'n')
+        this._fill_slot(2, key_name.charAt(0));
+      else
+        this._fill_slot(2, key_name.charAt(key_name.length - 1));
     }
       
     this._update_buffer();
