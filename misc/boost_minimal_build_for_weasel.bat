ade5b8094c916acc8e95a7fdafd8be2312ab70c4
chen.sst@gmail.com <chen.sst@gmail.com@b90aed45-4297-9963-ff9d-98b604ac09a9>
2011-12-02 19:21:34 +0000

    Add VS 2010 solution for Weasel.

--- misc/boost_minimal_build_for_weasel.bat
+++ misc/boost_minimal_build_for_weasel.bat
@@ -1,2 +1,4 @@
-bjam toolset=msvc-9.0 variant=release link=static threading=multi runtime-link=static stage --with-date_time --with-filesystem --with-system --with-regex --with-signals --with-thread
-bjam toolset=msvc-9.0 variant=release link=static threading=multi runtime-link=static address-model=64 --stagedir=stage_x64 stage --with-date_time --with-filesystem --with-system --with-regex --with-signals --with-thread
+rem bjam toolset=msvc-9.0 variant=release link=static threading=multi runtime-link=static stage --with-date_time --with-filesystem --with-system --with-regex --with-signals --with-thread
+rem bjam toolset=msvc-9.0 variant=release link=static threading=multi runtime-link=static address-model=64 --stagedir=stage_x64 stage --with-date_time --with-filesystem --with-system --with-regex --with-signals --with-thread
+bjam toolset=msvc-10.0 variant=release link=static threading=multi runtime-link=static stage --with-date_time --with-filesystem --with-system --with-regex --with-signals --with-thread
+bjam toolset=msvc-10.0 variant=release link=static threading=multi runtime-link=static address-model=64 --stagedir=stage_x64 stage --with-date_time --with-filesystem --with-system --with-regex --with-signals --with-thread
