561d55318082315d3a4a35b76f3e1953982a2180
chen.sst@gmail.com <chen.sst@gmail.com@b90aed45-4297-9963-ff9d-98b604ac09a9>
2011-12-12 13:07:54 +0000

    Simplifier.

--- misc/opencc/test/test_opencc.cc
+++ misc/opencc/test/test_opencc.cc
@@ -11,30 +11,29 @@ int main() {
         std::cerr << "error opening opencc." << std::endl;
         return 1;
     }
+    opencc_set_conversion_mode(od, OPENCC_CONVERSION_LIST_CANDIDATES);
+    const int MAX_LEN = 1000;
+    uint32_t inbuf[MAX_LEN];
+    uint32_t outbuf[MAX_LEN];
+    char result[MAX_LEN * 6];
     std::string line;
     while (std::getline(std::cin, line)) {
-        uint32_t *inbuf = new uint32_t[line.length() + 1];
         uint32_t *end = utf8::unchecked::utf8to32(line.c_str(), line.c_str() + line.length(), inbuf);
         *end = L'\0';
         uint32_t *inptr = inbuf;
         size_t inlen = end - inptr;
-        uint32_t *outbuf = new uint32_t[inlen + 1];
         uint32_t *outptr = outbuf;
-        size_t outlen = inlen;
+        size_t outlen = MAX_LEN;
         size_t converted = opencc_convert(od, &inptr, &inlen, &outptr, &outlen);
         *outptr = L'\0';
         if (!converted) {
             std::cerr << "error converting " << line << std::endl;
-            delete[] inbuf;
-            delete[] outbuf;
             opencc_close(od);
             return 1;
         }
-        char *result = new char[(outptr - outbuf) * 6 + 1];
         char *result_end = utf8::unchecked::utf32to8(outbuf, outptr, result);
         *result_end = '\0';
         std::cout << result << std::endl;
-        delete result;
     }
     opencc_close(od);
     return 0;
