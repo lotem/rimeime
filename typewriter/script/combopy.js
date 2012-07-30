dc4477fac8c5d6b66afb55ab67b1cc29c03b2ca9
chen.sst@gmail.com <chen.sst@gmail.com@b90aed45-4297-9963-ff9d-98b604ac09a9>
2012-07-30 10:45:40 +0000

    Update typewriter: new combo pinyin schema.

---
+++ typewriter/script/combopy.js
@@ -0,0 +1,137 @@
+var Combopy = Class.extend(Combo, {
+    key_map: {
+        blue:   { 'b': 'p', 'v': '<sub>m</sub>b', 
+                  'c': '<sub>zh</sub>f<sub>m</sub>', 'x': 'z<sub>zh</sub>', 
+                  't': 't', 'r': '<sub>n</sub>d', 
+                  'e': '<sub>ch</sub>l<sub>n</sub>', 'w': 'c<sub>ch</sub>',
+                  'g': 'k<sup>q</sup>', 'f': '<sub>r</sub>g<sup>j</sup>', 
+                  'd': '<sub>sh</sub>h<sup>x</sup><sub>r</sub>', 's': 's<sub>sh</sub>' },
+        green:  { 'n': 'er',
+                  'u': 'u', 'j': 'i', 'm': 'ü',
+                  'i': '\u026a<sub>ei</sub>', 'o': '<sub>ei</sub>o',
+                  'k': 'n<sub>ng</sub>', 'l': '<sub>ng</sub>e',
+                  'comma': ',<sub>ou</sub>', 'period': '<sub>ou</sub>.',
+                  },
+        yellow: { 'q': '-', 'a': '-', 'z': '-', 'y': '-', 'h': '-', 'p': '-',
+                  //'1': '1', '2': '2', '3': '3', '4': '4', '5': '5', 
+                  //'6': '6', '7': '7', '8': '8', '9': '9', '0': '0', 
+                  'semicolon': ';', 'slash': '/' },
+        red:    { 'space': 'a' }
+    },
+
+    key_order: 's w x f r v g t b d e c n j m u space k i comma l o period'.split(' '),
+
+    key_value: { 
+        'b': 'p', 'v': 'b', 'c': 'f', 'x': 'z', 
+        't': 't', 'r': 'd', 'e': 'l', 'w': 'c', 
+        'g': 'k', 'f': 'g', 'd': 'h', 's': 's', 
+        'space': 'a',
+        'n': 'er', 'j': 'i', 'u': 'u', 'm': 'ü', 
+        'i': 'y', 'o': 'o', 'k': 'n', 'l': 'e', 'comma': ',', 'period': '.'
+    },
+
+    pm2py: function (pm) {
+        var py = pm
+            .replace(/^a$/, "")
+            .replace(/^,$/, "COMMA")
+            .replace(/^\.$/, "PERIOD")
+            .replace(/,/, "w")
+            .replace(/\./, "e")
+            .replace(/^COMMA$/, ",")
+            .replace(/^PERIOD$/, ".")
+            .replace(/([aoeiuü])ne$/, "$1ng")
+            .replace(/ne$/, "eng")
+            .replace(/n$/, "en")
+            .replace(/([aoeiuü])en$/, "$1n")
+            .replace(/uyo$/, "ung")
+            .replace(/üwe$/, "iung")
+            .replace(/ü(a?)w$/, "i$1w")
+            .replace(/ay$/, "ai")
+            .replace(/yo$/, "ei")
+            .replace(/aw$/, "ao")
+            .replace(/we?$/, "ou")
+            .replace(/y/, "i")
+            .replace(/^ae$/, "a")
+            .replace(/^bf/, "m")
+            .replace(/^dl/, "n")
+            .replace(/^gh/, "r")
+            .replace(/^sgh?/, "r")
+            .replace(/^zbf?/, "w")
+            .replace(/^zf/, "zh")
+            .replace(/^cl/, "ch")
+            //.replace(/^sh/, "sh")
+            .replace(/^([bpfw])$/, "$1u")
+            .replace(/^([mdtnlgkh])$/, "$1e")
+            .replace(/^([zcs]h?|r)i$/, "$1")
+            .replace(/yi?/, "i")
+            .replace(/^[gz]i/, "ji")
+            .replace(/^[kc]i/, "qi")
+            .replace(/^[hs]i/, "xi")
+            .replace(/^[gz]ü/, "ju")
+            .replace(/^[kc]ü/, "qu")
+            .replace(/^[hs]ü/, "xu")
+            .replace(/^([zcs]h?|r)$/, "$1i")
+            .replace(/^i([aoeu])/, "y$1")
+            .replace(/^i/, "yi")
+            .replace(/^u([aoe])/, "w$1")
+            .replace(/^u/, "wu")
+            .replace(/ue([ni])/, "u$1")
+            .replace(/^wu([ni])/, "we$1")
+            .replace(/ung$/, "ong")
+            .replace(/^ü/, "yu")
+            //.replace(/üe/, "ue")
+            .replace(/iou$/, "iu")
+        ;
+
+        return py;
+    },
+
+    py2pm: function (py) {
+        var pm = py
+            .replace(/^([bpf])u$/, "$1")
+            .replace(/^([mdtnlgkh])e$/, "$1")
+            .replace(/^([zcs]h?|r)i$/, "$1")
+
+            .replace(/^ng$/, "eng")
+            .replace(/^([nl])ue$/, "$1ve")
+
+            .replace(/^j[uv]/, "gv")
+            .replace(/^q[uv]/, "kv")
+            .replace(/^x[uv]/, "hv")
+            .replace(/^ji?/, "gi")
+            .replace(/^qi?/, "ki")
+            .replace(/^xi?/, "hi")
+
+            .replace(/^zh/, "zf")
+            .replace(/^ch/, "cl")
+            //.replace(/^sh/, "sh")
+            .replace(/^r/, "gh")
+            .replace(/^n/, "dl")
+            .replace(/^m/, "bf")
+
+            .replace(/^yu/, "iu")
+            .replace(/^yi?/, "i")
+            .replace(/^wu?/, "u")
+
+            .replace(/([au])i$/, "$1y")
+            .replace(/ei$/, "yo")
+            .replace(/iao$/, "va,")
+            .replace(/iu$/, "v,")
+            .replace(/iong$/, "v,.")
+            .replace(/ong$/, "uyo")
+            .replace(/ao$/, "a,")
+            .replace(/ou$/, ",.")
+            .replace(/en/, "n")
+            .replace(/ng/, "ne")
+            .replace(/iu|v/, "ü")
+            .replace(/^a$/, "ae")
+
+            .split('').join('-')
+
+            .replace(/^e-r$/, "er")
+        ;
+
+        return pm;
+    }
+
+});
--- typewriter/script/zhung.js
+++ typewriter/script/zhung.js
@@ -1,125 +1,143 @@
 var Zhung = new Class({
 
-  key_map: {
-    /*
-    blue: {
-      '1': '1', '2': '2', '3': '3', '4': '4', '5': '5',
-      '6': '6', '7': '7', '8': '8', '9': '9', '0': '0' 
+  layers: [
+    {
+      yellow: {
+        '5': '5', '6': '6', '7': '7', '8': '8', '9': '9', 
+        'backquote': '`', 'hyphen': '-', 'equal': '=',
+        'bracketleft': '[', 'bracketright': ']', 'quote': '\''
+      },
+      blue: {
+        '1': '1', '2': '2', '3': '3', '4': '4', '0': '0',
+        'semicolon': '¨', 'comma': 'ˆ', 'period': '`', 'slash': '´'
+      },
+      green: {
+        'q': 'q', 'w': 'w', 'e': 'e', 'r': 'r', 't': 't',
+        'y': 'y', 'u': 'u', 'i': 'i', 'o': 'o', 'p': 'p',
+        'a': 'a', 's': 's', 'd': 'd', 'f': 'f', 'g': 'g',
+        'h': 'h', 'j': 'j', 'k': 'k', 'l': 'l', 
+        'z': 'z', 'x': 'x', 'c': 'c', 'v': 'v', 'b': 'b',
+        'n': 'n', 'm': 'm' 
+      },
+      red: {
+        'space': ' '
+      }
     },
-    yellow: {
-      'backquote': '`', 'hyphen': '-', 'equal': '=',
-      'bracketleft': '[', 'bracketright': ']',
-      'semicolon': ';', 'quote': '\'', 
-      'comma': ',', 'period': '.', 'slash': '/'
-    },
-    */
-    green: {
-      'q': 'ㄪ', 'w': 'ㄈ<sub>ㄤ</sub>', 'e': 'ㄇ<sub>ㄢ</sub>', 'r': 'ㄆ<sub>ㄡ</sub>', 't': 'ㄅ<sub>ㄠ</sub>',
-      'y': 'ㄉ<sub>ㄞ</sub>', 'u': 'ㄊ<sub>ㄟ</sub>', 'i': 'ㄋ<sub>ㄣ</sub>', 'o': 'ㄌ<sub>ㄥ</sub>', /* 'p': '', */
-      'a': 'ㄏ<sub>ㄚ</sub>', 's': 'ㄎ<sub>ㄛ</sub>', 'd': 'ㄍ<sub>ㄜ</sub>', 'f': 'ㄨ', 'g': 'ㄝ',
-      'h': 'ㄩ', 'j': 'ㄧ', 'k': 'ㄗ<sub>ˊ</sub>', 'l': 'ㄘ<sub>ˋ</sub>',
-      /* 'z': '', */ 'x': 'ㄖ', 'c': 'ㄕ', 'v': 'ㄔ', 'b': 'ㄓ',
-      'n': 'ㄦ<sub>ˆ</sub>', 'm': 'ㄙ<sub>˙</sub>' 
-    },
-    red: {
-      'space': ' '
+    { // uppercase
+      yellow: {
+        '1': '!', '2': '@', '3': '#', '4': '$', '5': '%',
+        '6': '^', '7': '&', '8': '*', '9': '(', '0': ')',
+        'backquote': '~', 'hyphen': '_', 'equal': '+',
+        'bracketleft': '{', 'bracketright': '}',
+        'semicolon': ':', 'quote': '\"', 
+        'comma': '<', 'period': '>', 'slash': '?'
+      },
+      green: {
+        'q': 'Q', 'w': 'W', 'e': 'E', 'r': 'R', 't': 'T',
+        'y': 'Y', 'u': 'U', 'i': 'I', 'o': 'O', 'p': 'P',
+        'a': 'A', 's': 'S', 'd': 'D', 'f': 'F', 'g': 'G',
+        'h': 'H', 'j': 'J', 'k': 'K', 'l': 'L', 
+        'z': 'Z', 'x': 'X', 'c': 'C', 'v': 'V', 'b': 'B',
+        'n': 'N', 'm': 'M' 
+      },
+      red: {
+        'space': ' '
+      }
     }
+  ],    
+
+  _preedit: '',
+  
+  _update_preedit: function() {
+    $('#buffer').val(this._format(this._preedit));
   },
 
-  buffer_size: 3,
-
-  _slot: ['', '', '', ''],
-
-  _slot_is_empty: function() {
-    for (var i = 0; i < this._slot.length; ++i) {
-      if (this._slot[i] != '')
-        return false;
+  _format: function(s) {
+    var t = '';
+    while (s != t) {
+      t = s;
+      s = t
+        .replace(/^\u00a8$/, ';')
+        .replace(/^\u02c6$/, ',')
+        .replace(/^\u0060$/, '.')
+        .replace(/^\u0060{2}$/, '..')
+        .replace(/^\u0060{3}$/, '...')
+        .replace(/^\u00b4$/, '/')
+        .replace(/([aoeiuyw])h5/g, '$1h1')
+        .replace(/([aoeiuyw])h6/g, '$1h2')
+        .replace(/([a-z]+)7/g, '$10')
+        .replace(/([a-z])\u02c6/g, '$12')
+        .replace(/([a-z])\u00b4/g, '$13')
+        .replace(/([a-z])\u0060/g, '$14')
+        .replace(/([a-z])\u00a8/g, '$10')
+        .replace(/([aoeiuy])([ptkhwlmn]|ng)([1-40])/g, '$1$3$2')
+        .replace(/([aoe])([iu])([1-40])/g, '$1$3$2')
+        .replace(/([aoe])([aoe]+)([1-40])/g, '$1$3$2')
+        .replace(/a2/g, '\u00e2')
+        .replace(/a3/g, '\u00e1')
+        .replace(/a4/g, '\u00e0')
+        .replace(/a0/g, '\u00e4')
+        .replace(/e2/g, '\u00ea')
+        .replace(/e3/g, '\u00e9')
+        .replace(/e4/g, '\u00e8')
+        .replace(/e0/g, '\u00eb')
+        .replace(/i2/g, '\u00ee')
+        .replace(/i3/g, '\u00ed')
+        .replace(/i4/g, '\u00ec')
+        .replace(/i0/g, '\u00ef')
+        .replace(/o2/g, '\u00f4')
+        .replace(/o3/g, '\u00f3')
+        .replace(/o4/g, '\u00f2')
+        .replace(/o0/g, '\u00f6')
+        .replace(/u2/g, '\u00fb')
+        .replace(/u3/g, '\u00fa')
+        .replace(/u4/g, '\u00f9')
+        .replace(/u0/g, '\u00fc')
+        .replace(/y2/g, '\u0177')
+        .replace(/y3/g, '\u00fd')
+        .replace(/y4/g, '\u1ef3')
+        .replace(/y0/g, '\u00ff')
+        .replace(/w2/g, '\u0175')
+        .replace(/w3/g, '\u1e83')
+        .replace(/w4/g, '\u1e81')
+        .replace(/w0/g, '\u1e85')
+        .replace(/([aoeiuyw])1/g, '$1')
+        /*
+        .replace(/([aoeiuyw])2/g, '\u0302$1')
+        .replace(/([aoeiuyw])3/g, '\u0301$1')
+        .replace(/([aoeiuyw])4/g, '\u0300$1')
+        .replace(/([aoeiuyw])0/g, '\u0308$1')
+        .replace(/([\u0300-\u030f])i/g, '$1\u0131')
+        */
+        ;
     }
-    return true;
-  },
-
-  _fill_slot: function(part, value) {
-    this._slot[part] = value;
-  },
-
-  _update_buffer: function() {
-    $('#buffer').val(this._slot.join(''));
-  },
-
-  _clear_buffer: function() {
-    this._slot = ['', '', '', ''];
-    $('#buffer').val('');
-  },
-
-  _clear_output: function() {
-    $('#output').val('');
-  },
-
-  process_escape: function() {
-    this._clear_output();
-    this._clear_buffer();
+    return s;
   },
 
   process_input: function(key, sink) {
     var dom_key = $('#key_' + key);
-    var key_name = $('span', dom_key).text();
+    var input = $('span', dom_key).text();
 
-    // shengdiau
-    if (!this._slot_is_empty() && 
-        ((key == 'space' || key == 'm' || key == 'k' || key == 'l' || key == 'n'))) {
-      if (key != 'space')
-        this._fill_slot(3, key_name.charAt(key_name.length - 1));
-      if (this._slot[0] && !this._slot[1] && !this._slot[2]) {
-        if (this._slot[0] == 'ㄅ') this._slot[0] = 'ㄠ';
-        else if (this._slot[0] == 'ㄆ') this._slot[0] = 'ㄡ';
-        else if (this._slot[0] == 'ㄇ') this._slot[0] = 'ㄢ';
-        else if (this._slot[0] == 'ㄈ') this._slot[0] = 'ㄤ';
-        else if (this._slot[0] == 'ㄉ') this._slot[0] = 'ㄞ';
-        else if (this._slot[0] == 'ㄊ') this._slot[0] = 'ㄟ';
-        else if (this._slot[0] == 'ㄋ') this._slot[0] = 'ㄣ';
-        else if (this._slot[0] == 'ㄌ') this._slot[0] = 'ㄥ';
-        else if (this._slot[0] == 'ㄍ') this._slot[0] = 'ㄜ';
-        else if (this._slot[0] == 'ㄎ') this._slot[0] = 'ㄛ';
-        else if (this._slot[0] == 'ㄏ') this._slot[0] = 'ㄚ';
-      }
-      var s = this._slot.join('');
-      sink(s);
-      this._clear_buffer();
+    if (input == ' ') {
+      sink(this._format(this._preedit));
+      this._preedit = '';
+    } else {
+      this._preedit += input;
     }
-    // jaiyin
-    else if (key == 'f' || key == 'j' || key == 'h') {
-      this._fill_slot(1, key_name.charAt(0));
-      if (key == 'j' || key == 'h') {
-        var shengmu = this._slot[0];
-        if (shengmu == 'ㄍ') this._fill_slot(0, 'ㄐ');
-        else if (shengmu == 'ㄎ') this._fill_slot(0, 'ㄑ');
-        else if (shengmu == 'ㄏ') this._fill_slot(0, 'ㄒ');
-      }
-    }
-    // shengmu
-    else if (this._slot_is_empty() && 
-             key != 'g' && key != 'n') {
-      this._fill_slot(0, key_name.charAt(0));
-    }
-    // yunmu
-    else {
-      if (key == 'n')
-        this._fill_slot(2, key_name.charAt(0));
-      else
-        this._fill_slot(2, key_name.charAt(key_name.length - 1));
-    }
-      
-    this._update_buffer();
+    this._update_preedit();
   },
 
   process_backspace: function() {
-    for (var i = 2; i >= 0; --i)
-      if (this._slot[i] != '') {
-        this._slot[i] = '';
-        break;
-      }
-    this._update_buffer();
+    if (this._preedit.length > 0) {
+      this._preedit = this._preedit.slice(0, -1);
+    }
+    this._update_preedit();
+  },
+
+  process_escape: function() {
+    this._preedit = '';
+    $('#buffer').val('');
+    $('#output').val('');
   }
 
 });
-
