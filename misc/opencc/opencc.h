e1dddc5be127ec8d833fdb36f0dd3b4bd494064a
chen.sst@gmail.com <chen.sst@gmail.com@b90aed45-4297-9963-ff9d-98b604ac09a9>
2013-04-21 08:07:49 +0000

    update opencc to version 0.4.2

--- misc/opencc/opencc.h
+++ misc/opencc/opencc.h
@@ -1,23 +1,33 @@
-/*
-* Open Chinese Convert
-*
-* Copyright 2010 BYVoid <byvoid.kcp@gmail.com>
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+/**
+ * @file
+ * OpenCC API.
+ *
+ * @license
+ * Open Chinese Convert
+ *
+ * Copyright 2010-2013 BYVoid <byvoid@byvoid.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef __OPENCC_H_
 #define __OPENCC_H_
+
+/**
+ * @defgroup opencc_api OpenCC API
+ * 
+ * API in C language
+ */
 
 #include "opencc_types.h"
 
@@ -25,107 +35,123 @@
 extern "C" {
 #endif
 
-/*
- * Headers from C standard library
+/**
+ * Filename of default Simplified to Traditional configuration.
+ *
+ * @ingroup opencc_api
  */
-
-/* Macros */
 #define OPENCC_DEFAULT_CONFIG_SIMP_TO_TRAD "zhs2zht.ini"
+
+/**
+ * Filename of default Traditional to Simplified configuration.
+ *
+ * @ingroup opencc_api
+ */
 #define OPENCC_DEFAULT_CONFIG_TRAD_TO_SIMP "zht2zhs.ini"
 
 /**
- * opencc_open:
- * @config_file: Location of configuration file.
- * @returns: A description pointer of the newly allocated instance of opencc.
+ * Makes an instance of opencc.
+ * Leave config_file to NULL if you do not want to load any configuration file.
  *
- * Make an instance of opencc.
- *
- * Note: Leave config_file to NULL if you do not want to load any configuration file.
- * 
+ * @param config_file Location of configuration file.
+ * @return            A description pointer of the newly allocated instance of
+ *                    opencc. On error the return value will be (opencc_t) -1.
+ * @ingroup opencc_api
  */
-opencc_t opencc_open(const char * config_file);
+opencc_t opencc_open(const char* config_file);
 
 /**
- * opencc_close:
- * @od: The description pointer.
- * @returns: 0 on success or non-zero number on failure.
+ * Destroys an instance of opencc.
  *
- * Destroy an instance of opencc.
- *
+ * @param od The description pointer.
+ * @return 0 on success or non-zero number on failure.
  */
 int opencc_close(opencc_t od);
 
 /**
- * opencc_convert:
- * @od: The opencc description pointer.
- * @inbuf: The pointer to the wide character string of the input buffer.
- * @inbufleft: The maximum number of characters in *inbuf to convert.
- * @outbuf: The pointer to the wide character string of the output buffer.
- * @outbufleft: The size of output buffer.
+ * Converts a UCS-4 string from *inbuf to *outbuf.
+ * Do not forget to assign **outbuf to L'\0' after called if you want to use it
+ * as a C-Style string.
  *
- * @returns: The number of characters of the input buffer that converted.
+ * @param od         The opencc description pointer.
+ * @param inbuf      The pointer to the UCS-4 string.
+ * @param inbufleft  The maximum number of characters in *inbuf to be converted.
+ * @param outbuf     The pointer to the output buffer.
+ * @param outbufleft The size of output buffer.
  *
- * Convert string from *inbuf to *outbuf.
- *
- * Note: Don't forget to assign **outbuf to L'\0' after called.
- *
+ * @return           The number of characters in the input buffer that has been
+ *                   converted.
+ * @ingroup opencc_api
  */
-size_t opencc_convert(opencc_t od, ucs4_t ** inbuf, size_t * inbufleft,
-		ucs4_t ** outbuf, size_t * outbufleft);
+size_t opencc_convert(opencc_t od,
+                      ucs4_t** inbuf,
+                      size_t* inbufleft,
+                      ucs4_t** outbuf,
+                      size_t* outbufleft);
 
 /**
- * opencc_convert_utf8:
- * @od: The opencc description pointer.
- * @inbuf: The UTF-8 encoded string.
- * @length: The maximum number of characters in inbuf to convert.
+ * Converts UTF-8 string from inbuf.
+ * This function returns an allocated C-Style string via malloc(), which stores
+ * the converted string.
+ * You should call free() to release allocated memory.
  *
- * @returns: The newly allocated UTF-8 string that converted from inbuf.
+ * @param od     The opencc description pointer.
+ * @param inbuf  The UTF-8 encoded string.
+ * @param length The maximum length of inbuf to convert. If length is set to -1,
+ *               the whole c-style string in inbuf will be converted.
  *
- * Convert UTF-8 string from inbuf. This function returns a newly allocated
- * c-style string via malloc(), which stores the converted string.
- * DON'T FORGET TO CALL free() to recycle memory.
- *
+ * @return       The newly allocated UTF-8 string that stores text converted
+ *               from inbuf.
+ * @ingroup opencc_api
  */
-char * opencc_convert_utf8(opencc_t t_opencc, const char * inbuf, size_t length);
+char* opencc_convert_utf8(opencc_t od, const char* inbuf, size_t length);
 
 /**
- * opencc_dict_load:
- * @od: The opencc description pointer.
- * @dict_filename: The name (or location) of the dictionary file.
- * @dict_type: The type of the dictionary.
+ * Loads a dictionary to default dictionary chain.
  *
- * @returns: 0 on success or non-zero number on failure.
+ * @param od             The opencc description pointer.
+ * @param dict_filename  The name (or location) of the dictionary file.
+ * @param dict_type      The type of the dictionary.
  *
- * Load a dictionary.
+ * @return               0 on success or non-zero number on failure.
  *
+ * @ingroup opencc_api
+ * @deprecated This function is not recommended to use and will be removed.
  */
-int opencc_dict_load(opencc_t t_opencc, const char * dict_filename,
-		opencc_dictionary_type dict_type);
-
-void opencc_set_conversion_mode(opencc_t t_opencc, opencc_conversion_mode conversion_mode);
+int opencc_dict_load(opencc_t od,
+                     const char* dict_filename,
+                     opencc_dictionary_type dict_type);
 
 /**
- * opencc_errno:
+ * Changes the mode of conversion.
  *
- * @returns: The error number.
+ * @param od               The opencc description pointer.
+ * @param conversion_mode  Conversion mode. Options are
+ *                         - OPENCC_CONVERSION_FAST
+ *                         - OPENCC_CONVERSION_SEGMENT_ONLY
+ *                         - OPENCC_CONVERSION_LIST_CANDIDATES
+ * @ingroup opencc_api
+ */
+void opencc_set_conversion_mode(opencc_t od,
+                                opencc_conversion_mode conversion_mode);
+
+/**
+ * Returns an opencc_convert_errno_t which describes the last error.
  *
- * Return an opencc_convert_errno_t which describes the last error that occured or
- * OPENCC_CONVERT_ERROR_VOID
- *
+ * @return The error type.
  */
 opencc_error opencc_errno(void);
 
 /**
- * opencc_perror:
- * @spec Prefix message.
+ * Prints the error message to stderr.
  *
- * Print the error message to stderr.
- *
+ * @param spec Prefix message.
+ * @ingroup opencc_api
  */
-void opencc_perror(const char * spec);
+void opencc_perror(const char* spec);
 
 #ifdef __cplusplus
-};
+}
 #endif
 
 #endif /* __OPENCC_H_ */
